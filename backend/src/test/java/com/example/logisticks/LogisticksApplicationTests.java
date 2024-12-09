b44abc75494cfaf604035b65d987d16b2aeb3f98
Chandak-Keshav <keshavchandak2408@gmail.com>
2024-12-10 02:25:54 +0530

    Added

--- backend/src/test/java/com/example/logisticks/LogisticksApplicationTests.java
+++ backend/src/test/java/com/example/logisticks/LogisticksApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.logisticks;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LogisticksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
